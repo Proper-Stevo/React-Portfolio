be3838390581723d558126a8137b967b61b05ef7
Stevo <steven.barrios92@gmail.com>
2022-11-01 21:52:36 -0700

    added utils and new form for contact me

--- src/components/pages/Contact/Index.js
+++ src/components/pages/Contact/Index.js
@@ -1,54 +1,95 @@
-import React, { useState } from 'react'
-import "./Contact.css";
+import React, { useState } from 'react';
+import './Contact.css';
 
-const InputContact = (props) => {
-  // Here we set two state variables for firstName and lastName using `useState`
-  const [name, setName] = useState('');
+// Here we import a helper function that will check if the email is valid
+import { checkPassword, validateEmail } from '../../../utils/helpers';
+
+function Form() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
   const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-const handleInputChange = (e) => {
-  // Getting the value and name of the input which triggered the change
-  e.preventDefault();
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    } else {
+      setPassword(inputValue);
+    }
+  };
 
-  setName(e.target.fname.value);
-  setEmail(e.target.femail.value);
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
 
-  console.log(name);
-  props.addContact(name,email);
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('Email or username is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    if (!checkPassword(password)) {
+      setErrorMessage(
+        `Choose a more secure password for the account: ${userName}`
+      );
+      return;
+    }
+    alert(`Hello ${userName}`);
 
-};
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName('');
+    setPassword('');
+    setEmail('');
+  };
 
   return (
     <div className='contactForm'>
-      <p>
-        Please Contact Me Here!
-        <hr />
-      </p>
+      <p> Contact Me Here: {userName}</p>
       <form className="form">
-        <input
-          value={name}
-          name="Name"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Name"
-        />
-        <br />
-        <br />
         <input
           value={email}
           name="email"
           onChange={handleInputChange}
-          type="text"
+          type="email"
           placeholder="Email"
         />
-        <br />
-        <br />
-        <button type="button" onClick={InputContact}>
-          Submit
-        </button>
+        < br />
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Username"
+        />
+        < br />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="Password"
+        />
+        < br />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
       </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
-  }
-export default InputContact;
+}
+
+export default Form;
